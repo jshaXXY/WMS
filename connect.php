2689ba67b12a7234205954d02a741b9f9663f90a
jshaXXY <jshaxuxiaoyu@qq.com>
2026-01-17 12:39:14 +0800

    welcome page and login page.

---
+++ connect.php
@@ -0,0 +1,6 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "WMS";
+    $connection = new mysqli($servername, $username, $password, $database);
---
+++ login.php
@@ -0,0 +1,4 @@
+<?php
+session_start();
+include "connect.php";
+global $connection;
